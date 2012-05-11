ca294570b6b5f03a72f7ddc1fa3ab0b1dd081072
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2012-05-11 12:09:14 -0500

    using new PlotManager in ModelAnalyzer.analyzeError

--- +tools/ModelAnalyzer.m
+++ +tools/ModelAnalyzer.m
@@ -190,11 +190,14 @@ classdef ModelAnalyzer < handle;
             end
         end
         
-        function h = analyzeError(this, mu, inputidx)
-            if nargin < 3
-                inputidx = [];
-                if nargin < 2
-                    mu = [];
+        function pm = analyzeError(this, mu, inputidx, pm)
+            if nargin < 4
+                pm = tools.PlotManager(false, 2, 3);
+                if nargin < 3
+                    inputidx = [];
+                    if nargin < 2
+                        mu = [];
+                    end
                 end
             end
             rmodel = this.rm;
@@ -216,11 +219,11 @@ classdef ModelAnalyzer < handle;
                 end
                 est = rmodel.ErrorEstimator.StateError;
             end
-            e = sqrt(sum((x - xr).^2,1));
-            xnorm = sqrt(sum(x.^2,1));
+            e = Norm.L2(x - xr);
+            xnorm = Norm.L2(x);
             erel = e./xnorm;
             estrel = est./xnorm;
-            xrnorm = sqrt(sum(xr.^2,1));
+            xrnorm = Norm.L2(xr);
             erelr = e./xrnorm;
             estrelr = est./xrnorm;
             
@@ -228,97 +231,61 @@ classdef ModelAnalyzer < handle;
             xrmin = xr-repmat(est,size(xr,1),1); xrplus = xr+repmat(est,size(xr,1),1);
             ymax = max([max(x(:)) max(xr(:)) max(xrmin(:)) max(xrplus(:))]);
             ymin = min([min(x(:)) min(xr(:)) min(xrmin(:)) min(xrplus(:))]);
-            h = [];
-            if this.SingleFigures
-                %subplot(1,2,1);
-                h(end+1)=figure;
-            else
-                h(1) = figure;
-                pos = get(0,'MonitorPosition');
-                set(h,'OuterPosition',pos(1,:));
-                subplot(2,3,1);
-            end
-            plot(t,general.Utils.preparePlainPlot(x));
-            xlabel('t');
+            
             if this.UseOutput
-                title(sprintf('The full system''s output (m=%d,time=%.3f)',size(x,1),time));
+                ti = sprintf('The full system''s output (m=%d,time=%.3f)',size(x,1),time);
             else
-                title(sprintf('The full system (d=%d,time=%.3f)',size(x,1),time));
+                ti = sprintf('The full system (d=%d,time=%.3f)',size(x,1),time);
             end
+            h = pm.nextPlot('fullsys',ti,'time','error');
+            plot(h,t,general.Utils.preparePlainPlot(x));
             axis([t(1) t(end) ymin ymax]);
-            if this.SingleFigures
-                h(end+1)=figure;
-                %subplot(1,2,2);
-            else
-                subplot(2,3,2);
-            end
+            
             if this.UseOutput
-                plot(t,general.Utils.preparePlainPlot(x),'b',...
-                    t,general.Utils.preparePlainPlot(xr),'r',...
-                    t,general.Utils.preparePlainPlot(xrmin),...
+                h = pm.nextPlot('outputs',...
+                    sprintf('Full+reduced system outputs with error bounds (r=%d,self time:%.3f, time with err est:%.3f)',...
+                    size(rmodel.V,2),timer_noerr,timer),...
+                    'time','y(t) / y^r(t)');
+                plot(h, t,general.Utils.preparePlainPlot(x),'b',...
+                    t, general.Utils.preparePlainPlot(xr),'r',...
+                    t, general.Utils.preparePlainPlot(xrmin),...
                     'r--',t,general.Utils.preparePlainPlot(xrplus),'r--');
-                title(sprintf('Full+reduced system outputs with error bounds (r=%d,self time:%.3f, time with err est:%.3f)',size(rmodel.V,2),timer_noerr,timer));
-                ylabel('y(t) / y^r(t)');
                 legend('Full system','Reduced system','Lower bound','Upper bound');
             else
-                plot(t,general.Utils.preparePlainPlot(xr),'r');
-                title(sprintf('Reduced system (r=%d,self time:%.3f, time with err est:%.3f)',size(rmodel.V,2),timer_noerr,timer));
-                ylabel('x^r(t)');
+                h = pm.nextPlot('redsys',sprintf('Reduced system (r=%d,self time:%.3f, time with err est:%.3f)',size(rmodel.V,2),timer_noerr,timer),...
+                    'time','x^r(t)');
+                plot(h, t, general.Utils.preparePlainPlot(xr),'r');
             end
-            xlabel('t'); 
             axis([t(1) t(end) ymin ymax]);
             
-            
             %% Absolute value plot
-            if this.SingleFigures
-                h(end+1)=figure;
-                %subplot(1,2,1);
-            else
-                subplot(2,3,3);
-            end
-            plot(t,xnorm,'b',t,xrnorm,'r',t,xrnorm-est,'r--',t,xrnorm+est,'r--');
-            xlabel('t');
-            title('The state variable norms');
+            h = pm.nextPlot('norms','The state variable norms','time','norms');
+            plot(h, t, xnorm,'b',t,xrnorm,'r',t,xrnorm-est,'r--',t,xrnorm+est,'r--');
             legend('Full system','Reduced system','Lower bound','Upper bound');
             
             % Error plots
-            if this.SingleFigures
-                %subplot(1,2,2);
-                h(end+1)=figure;
-            else
-                subplot(2,3,4);
-            end
-            semilogy(t,est,'b',t,e,'r');%,t,abs(e-est),'g');
-            xlabel('t');
-            title(sprintf('The state variable absolute errors.\nmean(e)=%g, mean(est)=%g',mean(e),mean(est)));
+            h = pm.nextPlot('abserrors',sprintf('The state variable absolute errors.\nmean(e)=%g, mean(est)=%g',mean(e),mean(est)),...
+                'time','errors');
+            semilogy(h,t,est,'b',t,e,'r');%,t,abs(e-est),'g');
             legend('Estimated error','True error');%,'Location','Best');
             
             % Relative Error plots
-            if this.SingleFigures
-                h(end+1)=figure;
-                %subplot(1,2,1);
-            else
-                subplot(2,3,5);
-            end
-            semilogy(t,estrel,'b',t,erel,'r');
-            xlabel('t');
-            title(sprintf(['The state variable relative errors (comp. to '...
+            h = pm.nextPlot('relerrors',sprintf(['The state variable relative errors (comp. to '...
                 'full solution)\nmean(e_{rel})=%g, mean(est_{rel})=%g'],...
-                mean(erel),mean(estrel)));
+                mean(erel),mean(estrel)),...
+                'time','errors');
+            semilogy(h,t,estrel,'b',t,erel,'r');
             legend('Estimated error','True error');%,'Location','Best');
             
-            if this.SingleFigures
-                %subplot(1,2,2);
-                h(end+1)=figure;
-            else
-                subplot(2,3,6);
-            end            
-            semilogy(t,erelr,'r',t,estrelr,'b');
-            xlabel('t');
-            title(sprintf(['The state variable relative errors (comp. to '...
+            h = pm.nextPlot('relerrors_red',sprintf(['The state variable relative errors (comp. to '...
                 'reduced solution)\nmean(ered_{rel})=%g, mean(estred_{rel})=%g'],...
-                mean(erelr),mean(estrelr)));
+                mean(erelr),mean(estrelr)),...
+                'time','errors');
+            semilogy(h,t,erelr,'r',t,estrelr,'b');
             legend('True error','Estimated error');%,'Location','Best');
+            if nargin < 4
+                pm.done;
+            end
         end
         
         %% %%%%%%%%%%%%%%%% DEIM stuff %%%%%%%%%%%%%%%%%%%%%%%%
