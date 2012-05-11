b480895b5aaa770ce3a742acafb6e2334b582eda
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2012-05-11 12:08:19 -0500

    added overrides for the common operations, so it can be used in error estimators (context: new DEIM estimator)

--- +dscomponents/LinearInputConv.m
+++ +dscomponents/LinearInputConv.m
@@ -11,16 +11,32 @@ classdef LinearInputConv < dscomponents.AInputConv
             this.B = B;
         end
         
-        function res = evaluate(this, t, mu)%#ok
+        function res = evaluate(this, ~, ~)
             res = this.B;
         end
         
-        function proj = project(this, V, W)%#ok
+        function proj = project(this, ~, W)
             proj = dscomponents.LinearInputConv(W' * this.B);
         end
         
         function copy = clone(this)
             copy = dscomponents.LinearInputConv(this.B);
+        end
+        
+        function prod = mtimes(this, other)
+            prod = dscomponents.LinearInputConv(this.B * other.B);
+        end
+        
+        function diff = minus(this, other)
+            diff = dscomponents.LinearInputConv(this.B - other.B);
+        end
+        
+        function diff = plus(this, other)
+            diff = dscomponents.LinearInputConv(this.B + other.B);
+        end
+        
+        function transp = ctranspose(this)
+            transp = dscomponents.LinearInputConv(this.B');
         end
     end
     
