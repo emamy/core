8f56bb15c003a0a031195ac087614cdbbac541dd
Timm Strecker <strecker.timm@googlemail.com>
2013-01-25 12:48:06 +0100

    vectorial semi-implicit euler implemented

--- +solvers/BaseCustomSolver.m
+++ +solvers/BaseCustomSolver.m
@@ -1,25 +1,27 @@
 classdef BaseCustomSolver < solvers.BaseSolver
-% BaseCustomSolver: Base class for all self-implemented solvers.
-%
-% Adds another abstraction layer to distinguish between self-implemented ode solvers and third-party
-% solvers.
-%
-% @author Daniel Wirtz @date 2011-05-31
-%
-% @new{0,4,dw,2011-05-31} Added this class.
-%
-% This class is part of the framework
-% KerMor - Model Order Reduction using Kernels:
-% - \c Homepage http://www.agh.ians.uni-stuttgart.de/research/software/kermor.html
-% - \c Documentation http://www.agh.ians.uni-stuttgart.de/documentation/kermor/
-% - \c License @ref licensing
+    % BaseCustomSolver: Base class for all self-implemented solvers.
+    %
+    % Adds another abstraction layer to distinguish between self-implemented ode solvers and third-party
+    % solvers.
+    %
+    % @author Daniel Wirtz @date 2011-05-31
+    %
+    % @change{0,7,ts,2013-01-24} accelerated method getCompTimes
+    %
+    % @new{0,4,dw,2011-05-31} Added this class.
+    %
+    % This class is part of the framework
+    % KerMor - Model Order Reduction using Kernels:
+    % - \c Homepage http://www.agh.ians.uni-stuttgart.de/research/software/kermor.html
+    % - \c Documentation http://www.agh.ians.uni-stuttgart.de/documentation/kermor/
+    % - \c License @ref licensing
     
     methods
         function this = BaseCustomSolver
             this = this@solvers.BaseSolver;
             this.Name = 'Base Custom Solver';
         end
-    
+        
         function [t, x] = solve(this, odefun, t, x0)
             % Solves the ode and wraps the actual solving method.
             %
@@ -108,70 +110,85 @@ classdef BaseCustomSolver < solvers.BaseSolver
                 if numel(t) == 2
                     times = t(1):this.MaxStep:t(2);
                     outputtimes = true(1,length(times));
-                else    
-                    % Find refinement indices
-                    idx = fliplr(find(t(2:end)-t(1:end-1)-this.MaxStep>100*eps));
-                    % If any "gaps" are present, fill up with MaxStep's
-                    if ~isempty(idx)
-                        for i=idx
-                            add = times(i):this.MaxStep:times(i+1);
-                            times = [times(1:i-1) add times(i+2:end)];
-                            outputtimes = [outputtimes(1:i) false(1,length(add)-2) outputtimes(i+1:end)];
-                        end
-                        tout = times(outputtimes);
-                        if numel(tout) ~= numel(t) || any(abs(tout - t) > 100*eps)
-                            error('Unexpected error: Computed time vector differs from the desired one.');
-                            t%#ok
-                        end
+                else
+                    len = length(t);
+                    n = max(round((t(2:len)-t(1:len-1))/this.MaxStep),1);
+                    m = ones(1,len);
+                    m(2:len) = cumsum(n) + m(2:len);% - (0:len-2);
+                    times = zeros(1,m(len));
+                    outputtimes = times;
+                    for i = 1:len-1
+                        times(m(i):m(i+1))=  t(i):(t(i+1)-t(i))/(n(i)):t(i+1); %linspace(t(i),t(i+1),n(i));
+                        outputtimes(m(i)) = 1;
                     end
+                    outputtimes(end) = 1;
                 end
+
+                
+                %                     % Find refinement indices
+                %                     idx = fliplr(find(t(2:end)-t(1:end-1)-this.MaxStep>100*eps));
+                %                     % If any "gaps" are present, fill up with MaxStep's
+                %
+                %                     if ~isempty(idx)
+                %                         for i=idx
+                %                             add = times(i):this.MaxStep:times(i+1);
+                %                             times = [times(1:i-1) add times(i+2:end)];
+                %                             outputtimes = [outputtimes(1:i) false(1,length(add)-2) outputtimes(i+1:end)];
+                %                         end
+                %                         tout = times(outputtimes);
+                %                         if numel(tout) ~= numel(t) || any(abs(tout - t) > 100*eps)
+                %                             error('Unexpected error: Computed time vector differs from the desired one.');
+                %                             t%#ok
+                %                         end
             end
         end
     end
-    
-    methods(Abstract, Access=protected)
-        % The abstract solve function for custom ODE solver implementations.
-        %
-        % In difference to the solvers.BaseSolver.solve method the times passed here are already
-        % matched to possible MaxStep restrictions (thus extended).
-        %
-        % Parameters:
-        % odefun: A function handle to the ode's function. Signature is
-        % 'odefun(t,x)'
-        % t: Either a two dimensional vector with t(1) < t(2) specifiying
-        % the start and end time, or a greater or equal to three
-        % dimensional, strictly monotoneously increasing vector explicitly
-        % setting the desired output times. Depending on the MaxStep
-        % property, the solver can work with a finer time step internally.
-        % x0: The initial value
-        %
-        % See also: getCompTimes
-        %
-        % @change{0,7,dw,2013-01-11} Passing the outputtimes parameter to the customSolve
-        % method in order to allow for more memory-efficient implementations.
-        x = customSolve(this, odefun, t, x0, outputtimes);
-    end
-    
-    events
-        % Gets fired before the solver starts.
-        % 
-        % Carries an SolverEventData instance with the Times field set to the actual times being
-        % used during solving.
-        %
-        % @attention This event is not fired when using a Matlab builtin solver.
-        %
-        % See also: solver.ode.SolverEventData
-        PreSolve;
-        
-        % Gets fired after the solver has finished.
-        %
-        % Carries an SolverEventData instance with the Times field set to the actual times being
-        % used during solving and the States field set to the corresponding state variable values.
-        %
-        % @attention This event is not fired when using a Matlab builtin solver.
-        %
-        % See also: solver.ode.SolverEventData
-        PostSolve;
-    end
-    
+end
+
+
+methods(Abstract, Access=protected)
+% The abstract solve function for custom ODE solver implementations.
+%
+% In difference to the solvers.BaseSolver.solve method the times passed here are already
+% matched to possible MaxStep restrictions (thus extended).
+%
+% Parameters:
+% odefun: A function handle to the ode's function. Signature is
+% 'odefun(t,x)'
+% t: Either a two dimensional vector with t(1) < t(2) specifiying
+% the start and end time, or a greater or equal to three
+% dimensional, strictly monotoneously increasing vector explicitly
+% setting the desired output times. Depending on the MaxStep
+% property, the solver can work with a finer time step internally.
+% x0: The initial value
+%
+% See also: getCompTimes
+%
+% @change{0,7,dw,2013-01-11} Passing the outputtimes parameter to the customSolve
+% method in order to allow for more memory-efficient implementations.
+x = customSolve(this, odefun, t, x0, outputtimes);
+end
+
+events
+% Gets fired before the solver starts.
+%
+% Carries an SolverEventData instance with the Times field set to the actual times being
+% used during solving.
+%
+% @attention This event is not fired when using a Matlab builtin solver.
+%
+% See also: solver.ode.SolverEventData
+PreSolve;
+
+% Gets fired after the solver has finished.
+%
+% Carries an SolverEventData instance with the Times field set to the actual times being
+% used during solving and the States field set to the corresponding state variable values.
+%
+% @attention This event is not fired when using a Matlab builtin solver.
+%
+% See also: solver.ode.SolverEventData
+PostSolve;
+end
+
 end
