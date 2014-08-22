54d3a707147af882a70a62653c9b6b8186a10cf8
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2014-08-22 11:57:11 +0200

    fixes, cleanup rewrote LinspaceSelector to also cater nonuniform trajectory lengths

--- +data/+selection/TimeSelector.m
+++ +data/+selection/TimeSelector.m
@@ -64,7 +64,7 @@ classdef TimeSelector < data.selection.ASelector
     end
     
     methods(Access=protected,Sealed)
-        function [xi, ti, mui] = select(this, model)
+        function [xi, ti, mui, fxi] = select(this, model)
             % Performs selection of samples adjusted to the apperances of different times.
             %
             % Parameters:
@@ -129,70 +129,8 @@ classdef TimeSelector < data.selection.ASelector
                     mui = [mui mu(:,ones(1,length(newt)))];
                 end
             end
+            fxi = [];
         end
-        
-%         function [xi, ti, mui] = select(this, model)
-%             % Performs selection of samples adjusted to the apperances of different times.
-%             %
-%             % Parameters:
-%             % model: The full model with the training data @type models.BaseFullModel
-%             %
-%             % Return values:
-%             % xi: The selected `x_i = x(t_i)` training data @type matrix
-%             % ti: The selected training times `t_i` @type rowvec
-%             % mui: The selected parameter samples `\mu_i` with which the states
-%             % `x_i` have been reached @type matrix
-%             %
-%             % @todo Adopt to current KerMor model data structure.
-%             
-%             % Build sn array similar to old one
-%             d = model.Data;
-%             tn = d.getNumTrajectories;
-%             tlen = length(model.Times);
-%             [x, mu] = d.getTrajectoryNr(1);
-%             xdim = size(x,1);
-%             sn = zeros(xdim+1+size(mu,2),tlen*tn);
-%             for i=1:tn
-%                 [x, mu] = d.getTrajectoryNr(i);
-%                 sn(:,1+(i-1)*tlen:i*tlen) = [model.Times; x; mu(:,ones(1,tlen))];
-%             end
-%             
-%             if (size(sn,2) > this.Size)
-%                 times = unique(sn(1,:));
-%                 tcnt = length(times);
-%                 occ = cell(1,tcnt);
-%                 num = zeros(1,tcnt);
-%                 
-%                 % Collect time apperances
-%                 for idx=1:tcnt
-%                     occ{idx} = find(sn(1,:) == times(idx));
-%                     num(idx) = length(occ{idx});
-%                 end
-%                 num = round((this.Size/sum(num)) * num);
-%                 
-%                 % Get random numtimes many samples for each time
-%                 selection = [];
-%                 s = RandStream.create('mt19937ar','Seed',this.Seed);
-%                 for idx=1:tcnt
-%                     sel = occ{idx};
-%                     selidx = s.randperm(length(sel));
-%                     selection(end+1:end+num(idx)) = sel(selidx(1:num(idx)));
-%                 end
-%                 
-%                 % Fill leftover sample places with linspaced leftovers
-%                 left = setdiff(1:size(sn,2),selection);
-%                 addidx = round(linspace(1,length(left),this.Size-length(selection)));
-%                 selection = sort([selection left(addidx)]);
-%                 atd = sn(:,selection);
-%             else
-%                 atd = sn;
-%             end
-%             
-%             % Transform atd back
-%             ti = atd(1,:);
-%             xi = atd(2:xdim+1,:);
-%             mui = atd(xdim+2:end,:);
-%         end
     end
     
     methods           
