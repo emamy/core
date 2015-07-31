5e899b48cf2557f20cfeaee4c0f1eee0de182ed2
Mylena Mordhorst <mylena.mordhorst@mechbau.uni-stuttgart.de>
2015-07-31 18:46:23 +0200

    relocate fix

--- +data/ProjectionSpace.m
+++ +data/ProjectionSpace.m
@@ -49,6 +49,15 @@ classdef ProjectionSpace < KerMorObject
             this.Size = size(V,2);
         end
         
+        function relocate(this, new_root)
+            if ~isempty(this.V)
+                this.V.relocate(new_root);
+            end
+            if ~isempty(this.W)
+                this.W.relocate(new_root);
+            end
+        end
+        
         function set.V(this, value)
             if ~isa(value,'data.FileMatrix') && (~isa(value, 'double') || ~ismatrix(value))
                 error('value must be a valid matrix of type double or a data.FileMatrix');
